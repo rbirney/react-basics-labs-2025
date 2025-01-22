cf372203220af37a6b2793bd06238953eb9b17a7
rbirney <rbirney@wit.ie>
2025-01-22 12:34:53 +0000

    Tasky lab 1 exercises done

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -5,10 +5,8 @@ function App() {
   return (
     <div className="container">
       <h1>Tasky</h1>
-      <Task title="Dishes" deadline="Today" />
-      <Task title="Laundry" deadline="Tomorrow">
-        Fold laundry and put away
-      </Task>
+      <Task title="Dishes" deadline="Today" description="Empty dishwasher" />
+      <Task title="Laundry" deadline="Tomorrow" description="Fold laundry and put away" />
       <Task title="Tidy" deadline="Today" />
     </div>
   );
